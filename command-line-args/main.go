8e0b6fa4948fdc8cf649a5fb6119986a6b3d1d3e
sepideh.mahmoudi <sepideh.mahmoudi@digikala.com>
2023-07-13 12:50:16 +0330

    add command-line-args

---
+++ command-line-args/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	// "fmt"
+	// "os"
+	// "strings"
+)
+
+// func main() {
+// 	var s, sep string // if you dont init explicitly this variables init implicitly for integers : 0 and strings : ""
+// 	for i := 0; i < len(os.Args); i++ {
+// 		s += sep + os.Args[i]
+// 		sep = " "
+// 	}
+// 	fmt.Println(s)
+// }
+func main () {
+	// s, sep =: "", ""  short form of initialization.
+	// var s string
+	// var s = ""
+	// var s string = ""
+
+	// var pow = []int{2,3,4,5,6,7}
+	// var str = []string {"q", "w", "e", "r"}
+	// for _, v := range pow[1:] { // we use blank identifier when use range. and we cant pass arrays without range and completely to this kind of for.we must 
+	// 	//pass array in for i=0;i<len(array);i++ for iterating content of values.
+	// 	fmt.Println(v)
+	// }
+		// fmt.Println(pow)  show in this form : [2,3,4,5,6,7]
+		// fmt.Println(strings.Join(str , "joon")) // add joon to each elements of str array, you should use string arrays.
+
+}
